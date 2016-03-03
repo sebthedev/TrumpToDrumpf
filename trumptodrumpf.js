39760448564103dc9e3fcde371158b5c66bf94e5
Sebastian Hallum Clarke <s@zibity.com>
2016-03-03 14:43:56 -0500

    Initial commit

---
+++ trumptodrumpf.js
@@ -0,0 +1,34 @@
+// I stole all this stuff from here: https://github.com/panicsteve/cloud-to-butt/blob/master/Source/content_script.js
+
+walk(document.body);
+
+function walk(node) {
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	var child, next;
+
+	switch (node.nodeType) {
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while (child) {
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode)  {
+	var v = textNode.nodeValue;
+
+    v = v.replace(/\bTrump\b/g, "Drumpf");
+		v = v.replace(/\btrump\b/g, "drumpf");
+
+	textNode.nodeValue = v;
+}
